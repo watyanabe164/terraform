f21ca9e4cf7f449ded1e885f9ceeb50e67651962
Hiroshi Watanabe <watyanabe164@gmail.com>
2023-06-09 19:12:33 +0900

    修正

--- main.tf
+++ main.tf
@@ -36,6 +36,7 @@ resource "azurerm_subnet" "example" {
   address_prefixes     = ["10.0.0.0/24"]
 }
 
+# NICの作成
 resource "azurerm_network_interface" "example" {
   count               = 3
   name                = "example-nic-${count.index}"
@@ -46,5 +47,54 @@ resource "azurerm_network_interface" "example" {
     name                          = "example-ipconfig"
     subnet_id                     = azurerm_subnet.example.id
     private_ip_address_allocation = "Dynamic"
+  }
+}
+
+# VMの作成
+resource "azurerm_virtual_machine" "example" {
+  count                 = 3
+  name                  = "example-vm-${count.index}"
+  location              = azurerm_resource_group.example.location
+  resource_group_name   = azurerm_resource_group.example.name
+  network_interface_ids = [azurerm_network_interface.example[count.index].id]
+
+  vm_size              = "Standard_DS1_v2"
+  delete_os_disk_on_termination = true
+  delete_data_disks_on_termination = true
+
+  storage_image_reference {
+    publisher = "Canonical"
+    offer     = "UbuntuServer"
+    sku       = "18.04-LTS"
+    version   = "latest"
+  }
+
+  storage_os_disk {
+    name              = "osdisk"
+    caching           = "ReadWrite"
+    create_option     = "FromImage"
+    managed_disk_type = "Standard_LRS"
+  }
+
+  os_profile {
+    computer_name  = "example-vm-${count.index}"
+    admin_username = "adminuser"
+    admin_password = "Password1234!"
+  }
+
+  os_profile_linux_config {
+    disable_password_authentication = false
+  }
+
+  boot_diagnostics {
+    storage_account_uri = azurerm_storage_account.example.primary_blob_endpoint
+  }
+
+  provisioner "remote-exec" {
+    inline = [
+      "sudo apt-get update",
+      "sudo apt-get install -y nginx",
+      "sudo service nginx start"
+    ]
   }
 }
